23243fe7d2b82d104ce0e475a8a37fa95ca05589
kazi Akib Rayhan <kaziakibrayhan2018@gmail.com>
2023-01-06 18:36:21 +0600

    get data to mongodb and limit 3 data to show client side when hit the created api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("colors");
 require("dotenv").config();
 const app = express();
@@ -11,10 +11,55 @@ app.use(cors());
 app.use(express.json());
 // MongDb url and client.
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.7mdpwuk.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-app.get("/", (req, res) => res.send("Monika's Therapy Server Running"));
+async function dbConnect() {
+  try {
+    await client.connect();
+    console.log("Connected to Database".bgGreen.bold);
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+}
+dbConnect();
+// Database & Collection
+
+const serviceCollection = client
+  .db("monikasTherapy")
+  .collection("therapyServices");
+
+// api end point
+app.get("/", async (req, res) => {
+  try {
+    res.send({
+      success: true,
+      message: "Monika's Therapy Server Running",
+    });
+  } catch (error) {
+    console.log(error.name.bgRed, error.message.bold);
+  }
+});
+
+app.get("/homeService", async (req, res) => {
+  const cursor = serviceCollection.find({}).limit(3);
+  const homeServices = await cursor.toArray();
+
+  res.send({
+    success: true,
+    message: "Successfully got the data",
+    data: homeServices,
+  });
+});
+
+
+
 
 app.listen(port, () =>
-  console.log(`Monika's Therapy listening at http://localhost:${port}`.cyan.bold)
+  console.log(
+    `Monika's Therapy listening at http://localhost:${port}`.cyan.bold
+  )
 );
